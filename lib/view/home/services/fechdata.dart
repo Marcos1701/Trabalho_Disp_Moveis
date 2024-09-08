1ccb4c95b6947f47f13b4d7e509345aaf2ac4f0a
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2024-09-08 13:11:47 -0300

    :tada: Aparente funcinal Grazadeussssss

--- lib/view/home/services/fechdata.dart
+++ lib/view/home/services/fechdata.dart
@@ -21,51 +21,58 @@ Future<List<TempleModel>> getTempleList(LatLng latLng, context) async {
     "shopping_mall",
     "supermarket",
   ];
-  String apiKey = dotenv.env['ApiKey'] ?? "";
+  String apiKey = await FlutterConfig.get('ApiKey');
 
-  if (apiKey == "") {
-    apiKey = await FlutterConfig.get('ApiKey');
+  if (apiKey.isEmpty) {
+    logger.e(
+        'API KEY não encontrada, resumindo a situação: A aplicação foi de arrasta para cima!!!');
+    throw Exception('API KEY não encontrada');
   }
 
   List<TempleModel> templeList = [];
 
   for (int i = 0; i < types.length; i++) {
-    try {
-      http.Response response = await http.get(Uri.parse(
-          'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latLng.latitude},${latLng.longitude}&type=${types[i]}&rankby=distance&key=$apiKey&language=pt-BR'));
+    // try {
 
-      Map<String, dynamic> json = {};
-      json = jsonDecode(response.body);
-      if (json['status'] != 'OK') {
-        // throw Exception(json['error_message']);
-        logger.e(json['error_message']);
-        return [];
-      }
-      var result = json['results'];
-      // List<TempleModel> templeList = [];
-      // logger.d(json);R
+    http.Response response = await http.get(Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latLng.latitude},${latLng.longitude}&type=${types[i]}&rankby=distance&key=$apiKey&language=pt-BR'));
 
-      for (var element in result) {
-        logger.d(element);
-        templeList.add(TempleModel(
-            name: element['name'],
-            address: element['vicinity'], //endereço
-            latLng: LatLng(
-                element['geometry']['location']['lat'], //latitude e longitude
-                element['geometry']['location']['lng']),
-            imageUrl: element['icon'],
-            placesId: element['place_id'],
-            types: element['types']));
-      }
-    } catch (e) {
-      logger.e(
-        e,
-        stackTrace: StackTrace.current,
-        error: e.toString(),
-        time: DateTime.now(),
-      ); //exibe uma mensagem de erro na tela, como se fosse um log
-      continue;
+    Map<String, dynamic> json = {};
+    json = jsonDecode(response.body);
+    if (json['status'] != 'OK') {
+      // throw Exception(json['error_message']);
+      logger.e(json['error_message']);
+      logger.d(
+          'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latLng.latitude},${latLng.longitude}&type=${types[i]}&rankby=distance&key=$apiKey&language=pt-BR');
+      return [];
     }
+    var result = json['results'];
+    // List<TempleModel> templeList = [];
+    // logger.d(json);R
+
+    for (var element in result) {
+      // logger.d(element);
+
+      element['types'] = element['types'].toString().toLowerCase().split(',');
+
+      templeList.add(TempleModel(
+          name: element['name'],
+          address: element['vicinity'], //endereço
+          latLng: LatLng(
+              element['geometry']['location']['lat'], //latitude e longitude
+              element['geometry']['location']['lng']),
+          imageUrl: element['icon'],
+          placesId: element['place_id'],
+          types: element['types']));
+    }
+    // } catch (e) {
+    //   logger.e(
+    //     e,
+    //     error: e.toString(),
+    //     time: DateTime.now(),
+    //   ); //exibe uma mensagem de erro na tela, como se fosse um log
+    //   continue;
+    // }
   }
   return templeList;
 }
--- lib/view/home/view.dart
+++ lib/view/home/view.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -8,6 +7,7 @@ import 'package:trabalho_loc_ai/view/home/models/model_locations.dart';
 import 'package:trabalho_loc_ai/view/home/services/fechdata.dart';
 import 'package:trabalho_loc_ai/view/home/services/bitmaphelper.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:custom_info_window/custom_info_window.dart';
 
 class LocationMap extends StatefulWidget {
   const LocationMap({super.key});
@@ -18,11 +18,11 @@ class LocationMap extends StatefulWidget {
 
 class LocationMapState extends State<LocationMap>
     with TickerProviderStateMixin {
-  
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
-
   final Set<Marker> _markers = {};
+  final CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
 
   // Json com os icones para cada tipo de local
   final iconsFromJson = {
@@ -41,35 +41,73 @@ class LocationMapState extends State<LocationMap>
 
   LatLng? _lastMapPosition;
 
+  TempleModel? _selectedTemple;
+
+  @override
+  void dispose() {
+    _customInfoWindowController.dispose();
+    super.dispose();
+  }
+
   // metodo para inicializar o mapa
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+    _customInfoWindowController.googleMapController = controller;
+  }
+
+  Widget buildInfoWindow(TempleModel temple) {
+    return Column(children: [
+      Text(temple.name),
+      Text(temple.address),
+      //botão para favoritar o local, com icone de estrela e fundo amarelo
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.star),
+        color: Colors.amber,
+        iconSize: 30,
+        tooltip: 'Favoritar',
+        splashRadius: 20,
+        splashColor: Colors.amber,
+        highlightColor: Colors.amber,
+      )
+    ]);
   }
 
   void _getData() async {
     if (_lastMapPosition != null) {
-      List<TempleModel> templeList = [];
-
-      await Future.wait([
-        getTempleList(_lastMapPosition!, context)
-            .then((value) => templeList = value),
-        // BitmapDescriptorHelper.getBitmapDescriptorFromAsset(
-        //         'assets/images/marker.png', context)
-        //     .then((value) => _mapStyle = value)
-      ]);
+      List<TempleModel> templeList =
+          await getTempleList(_lastMapPosition!, context);
 
       for (int i = 0; i < templeList.length; i++) {
+        //verifica se o local já existe no mapa
+        if (_markers
+            .any((marker) => marker.markerId == MarkerId(templeList[i].name))) {
+          continue;
+        }
+
         setState(() {
           _markers.add(
             Marker(
-              markerId: MarkerId(templeList[i].name),
+              markerId: MarkerId(templeList[i].placesId),
               position: templeList[i].latLng,
-              infoWindow: InfoWindow(
-                title: templeList[i].name,
-                snippet: templeList[i].address,
-                // onTap: () => Navigator.pushNamed(context, 'details',
-                //     arguments: templeList[i]),
-              ),
+              // infoWindow: InfoWindow(
+              //     title: templeList[i].name,
+              //     snippet: templeList[i].address,
+
+              //     //     arguments: templeList[i]),
+              //     ),
+              onTap: () {
+                print("Apertou, ein? Abestado");
+                setState(() {
+                  _selectedTemple = templeList[i];
+                });
+                _customInfoWindowController.addInfoWindow!(
+                  buildInfoWindow(templeList[i]),
+                  templeList[i].latLng,
+                );
+              },
+              icon: iconsFromJson[templeList[i].types.first] ??
+                  BitmapDescriptor.defaultMarker,
               // icon:
             ),
           );
@@ -108,65 +146,122 @@ class LocationMapState extends State<LocationMap>
     return;
   }
 
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: 200,
+          child: _selectedTemple != null
+              ? Column(
+                  children: [
+                    // Image.network(_selectedTemple!.imageUrl),
+                    Text(_selectedTemple!.name),
+                    Text(_selectedTemple!.address),
+                    TextButton(
+                      onPressed: () {
+                        // favoritar o local
+                      },
+                      child: const Text('Favoritar'),
+                    ),
+                  ],
+                )
+              : const Text('Nenhum local selecionado'),
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     super.initState();
-    requestPermission();
 
     // carrega o arquivo de estilo do mapa
     setState(() {
       rootBundle.loadString('assets/map_style.txt').then((string) {
         _mapStyle = string;
       });
+
+      requestPermission();
     });
   }
 
-  // Metodo responsável por carregar as localizações e os marcadores dos restaurantes
-  loadData() async {}
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+    _customInfoWindowController.onCameraMove!();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: const CameraPosition(
-        target: LatLng(-20.508, -54.617),
-        zoom: 18,
-      ),
-      myLocationEnabled: true,
-      myLocationButtonEnabled: true,
-      style: _mapStyle,
-      mapType: MapType.normal,
-      zoomControlsEnabled: false,
-      zoomGesturesEnabled: true,
-      markers: Set<Marker>.of(_markers),
-      onLongPress: _onMapLongPressed,
-    ));
-  }
-
-  // Metodo para adicionar o marcador no click
-  void _onMapLongPressed(LatLng latLng) {
-    // evento disparado quando o mapa é pressionado e segurado por alguns segundos
-    Future.delayed(const Duration(seconds: 1), () {
-      // adiciona um novo marcador no click
-      setState(() {
-        _markers.add(Marker(
-          markerId: MarkerId(_markers.length.toString()),
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-          position: latLng,
-          infoWindow: const InfoWindow(
-            title: "Novo Marcador",
-          ),
-        ));
-
-        _controller.future.then((GoogleMapController controller) {
-          controller
-              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-            target: latLng,
+      body: Stack(children: <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _lastMapPosition ?? const LatLng(0, 0),
             zoom: 18,
-          )));
-        });
-      });
-    });
+          ),
+          myLocationEnabled: true,
+          myLocationButtonEnabled: false,
+          style: _mapStyle,
+          mapType: MapType.normal,
+          zoomControlsEnabled: false,
+          zoomGesturesEnabled: true,
+          indoorViewEnabled: false,
+          markers: Set<Marker>.of(_markers),
+          // onLongPress: _onMapLongPressed,
+          onCameraMove: _onCameraMove,
+          onTap: (LatLng latLng) {
+            _customInfoWindowController.hideInfoWindow!();
+            _selectedTemple = null;
+          },
+        ),
+        CustomInfoWindow(
+          controller: _customInfoWindowController,
+          height: 200,
+          width: 200,
+          offset: 50,
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          requestPermission();
+        },
+        tooltip: 'Localização',
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.my_location),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              //verifica se já está na home, se não estiver, navega para a home
+              onPressed: () => Navigator.pushNamed(context, 'home'),
+              icon: const Icon(Icons.home),
+              tooltip: 'Home',
+            ),
+            //aba para visualizar os estabelecimentos próximos, em forma de lista
+            const IconButton(
+                onPressed: null,
+                icon: Icon(Icons.list),
+                tooltip: 'Lista de estabelecimentos'),
+            const IconButton(
+              onPressed: null,
+              icon: Icon(Icons.favorite),
+              tooltip: 'Favoritos',
+            ),
+            const IconButton(
+              onPressed: null,
+              icon: Icon(Icons.account_circle),
+              tooltip: 'Minha conta',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+    );
   }
 }
